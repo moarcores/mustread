a039abcd747b389bdd86570547760e33ebc85440
toffty <dendimart@gmail.com>
2021-11-05 18:13:33 +0300

    Implement routing, header component and home page component

---
+++ frontend/src/components/Navbar/navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes, {shape} from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import './Navbar.scss';
+
+export const Navbar = ({ links }) => {
+    return (
+        <div className="navbar">
+            {links.map(link => (
+                <Link key={link.title} to={link.url} className="navbar__item">
+                    {link.title}
+                </Link>
+            ))}
+        </div>
+    )
+};
+
+Navbar.propTypes = {
+    links: PropTypes.arrayOf({
+        link: shape({
+            url: PropTypes.string,
+            title: PropTypes.string
+        })
+    })
+}
---
+++ frontend/src/state/bookSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const booksSlice = createSlice({
+    name: 'books',
+    initialState: {
+        books: {},
+        selectedBook: '',
+        favoriteBooks: [],
+    },
+    reducers: {
+
+    }
+})
+
+export default booksSlice.reducer;
