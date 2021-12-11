e335f058cb5f4322ad2a1fee782bf220550d12b4
sergeielnikov <elnikov719@gmail.com>
2021-12-11 21:51:58 +0300

    fix

--- server/mustread/src/main/kotlin/com/itmo/mustread/users/entity/User.kt
+++ server/mustread/src/main/kotlin/com/itmo/mustread/users/entity/User.kt
@@ -2,8 +2,13 @@ package com.itmo.mustread.users.entity
 
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 class User : UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private val username: String? = null
     private val password: String? = null
     private val authority: List<GrantedAuthority>? = null
