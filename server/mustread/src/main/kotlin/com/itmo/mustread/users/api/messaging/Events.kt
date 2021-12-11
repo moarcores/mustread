f773544d3063da5fbe0ceb9577f98e794bd477bb
Nalgorithm <niklogvinenko@yandex-team.ru>
2021-12-11 20:43:10 +0300

    delete unused code

--- server/mustread/src/main/kotlin/com/itmo/mustread/users/api/messaging/Events.kt
+++
@@ -1,7 +0,0 @@
-package com.itmo.mustread.users.api.messaging
-
-import com.itmo.mustread.users.api.model.UserModel
-
-data class UserCreatedEvent(val user: UserModel)
-
-data class UserDeletedEvent(val username: String)
